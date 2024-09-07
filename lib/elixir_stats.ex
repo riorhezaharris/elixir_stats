f828c3f4f69710ad53edd307d980a6f075f58c59
Rio Rheza Harris <harris.riorheza@gmail.com>
2024-09-07 19:42:13 +0700

    initialize project

---
+++ lib/elixir_stats.ex
@@ -0,0 +1,18 @@
+defmodule ElixirStats do
+  @moduledoc """
+  Documentation for `ElixirStats`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirStats.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_stats_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirStatsTest do
+  use ExUnit.Case
+  doctest ElixirStats
+
+  test "greets the world" do
+    assert ElixirStats.hello() == :world
+  end
+end
